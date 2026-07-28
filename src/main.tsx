423749e264428306d9fc1406e842820aaa7d2f13
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-07-28 19:27:44 +0500

    Make full analytics dashboard the application entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AnalyticsApp from './AnalyticsApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AnalyticsApp />
   </React.StrictMode>,
 );
