041d45c180da325ccc1d24991775d72d8a6cbd1e
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-07-28 19:29:26 +0500

    Load analytics dashboard styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AnalyticsApp from './AnalyticsApp';
 import './styles.css';
+import './analytics.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
