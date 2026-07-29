2ff419a1810b5ff0a8b09eae3406c4b783a6f99f
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-07-30 02:06:59 +0500

    Remove duplicate conversion cards from analytics workspace

--- src/components/AnalyticsWorkspace.tsx
+++ src/components/AnalyticsWorkspace.tsx
@@ -1,8 +1,10 @@
 import V36Dashboard from './V36Dashboard';
 import DetailedConversionMatrices from './DetailedConversionMatrices';
+import RemoveLegacyConversionCards from './RemoveLegacyConversionCards';
 
 export default function AnalyticsWorkspace() {
   return <div className="stack">
+    <RemoveLegacyConversionCards />
     <V36Dashboard />
     <DetailedConversionMatrices days={7} />
   </div>;
