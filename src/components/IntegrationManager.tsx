8610abeab666760927ec37e6ab8a13e6e3bd7000
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-07-29 22:40:09 +0500

    Apply integrations page journal override

--- src/components/IntegrationManager.tsx
+++ src/components/IntegrationManager.tsx
@@ -1,6 +1,7 @@
 import CommunicationIntegrations from './CommunicationIntegrations';
 import IntegrationWorkspace from './IntegrationWorkspace';
 import '../integration-catalog.css';
+import '../integration-page-overrides.css';
 
 export default function IntegrationManager() {
   return <div className="stack integrations-page-without-journal">
