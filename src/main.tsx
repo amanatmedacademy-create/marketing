847448c9135b5e23281006b7a2f76019b0eb899a
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-07-29 11:30:27 +0500

    Require Google session for the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AnalyticsApp from './AnalyticsApp';
+import AuthGate from './components/AuthGate';
 import './styles.css';
 import './analytics.css';
+import './auth.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AnalyticsApp />
+    <AuthGate>
+      <AnalyticsApp />
+    </AuthGate>
   </React.StrictMode>,
 );
