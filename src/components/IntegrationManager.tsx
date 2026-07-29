60cbdd7f5d7520288f980563500d9536f38ba33d
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-07-29 22:39:45 +0500

    Remove embedded synchronization journal from integrations page

--- src/components/IntegrationManager.tsx
+++ src/components/IntegrationManager.tsx
@@ -3,7 +3,7 @@ import IntegrationWorkspace from './IntegrationWorkspace';
 import '../integration-catalog.css';
 
 export default function IntegrationManager() {
-  return <div className="stack">
+  return <div className="stack integrations-page-without-journal">
     <IntegrationWorkspace />
     <CommunicationIntegrations />
   </div>;
