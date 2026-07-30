d552df1edbd05f2cd4387099add9b05634b473f9
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-07-30 22:06:23 +0500

    feat: enable WABA connector in integration catalog

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import AuthGate from './components/AuthGate';
 import MetaOAuthLauncher from './components/MetaOAuthLauncher';
 import AdvertisingCatalogExpansion from './components/AdvertisingCatalogExpansion';
+import WabaCatalogActivation from './components/WabaCatalogActivation';
 import IntegrationCatalogNormalizer from './components/IntegrationCatalogNormalizer';
 import AnalyticsTableColorizer from './components/AnalyticsTableColorizer';
 import ThemeToggle from './components/ThemeToggle';
@@ -41,6 +42,7 @@ function Root() {
     <AnalyticsTableColorizer />
     <MetaOAuthLauncher />
     <AdvertisingCatalogExpansion />
+    <WabaCatalogActivation />
     <IntegrationCatalogNormalizer />
   </>;
 }
