51f03a25dcc405aa9fb79ce3987ebd62d4c6a8aa
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-07-30 14:01:00 +0500

    Подключить удаление дублей телефонии

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import AuthGate from './components/AuthGate';
 import MetaOAuthLauncher from './components/MetaOAuthLauncher';
 import AdvertisingCatalogExpansion from './components/AdvertisingCatalogExpansion';
+import TelephonySectionDeduplicator from './components/TelephonySectionDeduplicator';
 import AnalyticsTableColorizer from './components/AnalyticsTableColorizer';
 import MarketingPlatform from './MarketingPlatform';
 import MarketingOS from './pages/MarketingOS';
@@ -33,6 +34,7 @@ function Root() {
     <AnalyticsTableColorizer />
     <MetaOAuthLauncher />
     <AdvertisingCatalogExpansion />
+    <TelephonySectionDeduplicator />
   </>;
 }
 
