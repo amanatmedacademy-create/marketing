1315af9a5b007af4d4eecca680949fa62fb87c7d
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-01 03:43:20 +0500

    feat: enable Meta webhook controller

--- apps/api/src/modules/whatsapp/whatsapp.module.ts
+++ apps/api/src/modules/whatsapp/whatsapp.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RolesGuard } from '../../common/auth/roles.guard.js';
+import { MetaWebhookController } from './meta-webhook.controller.js';
+import { MetaWebhookService } from './meta-webhook.service.js';
 import { WhatsAppController } from './whatsapp.controller.js';
 import { WhatsAppService } from './whatsapp.service.js';
 
 @Module({
-  controllers: [WhatsAppController],
-  providers: [WhatsAppService, RolesGuard],
-  exports: [WhatsAppService],
+  controllers: [WhatsAppController, MetaWebhookController],
+  providers: [WhatsAppService, MetaWebhookService, RolesGuard],
+  exports: [WhatsAppService, MetaWebhookService],
 })
 export class WhatsAppModule {}
