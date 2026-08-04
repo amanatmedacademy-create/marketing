681b53e341f34e04ff7f9db49200076321a7120e
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-04 14:00:02 +0500

    feat: embed TikTok campaign analytics

--- apps/web/src/modules/ads/AdsWorkspaceWithMeta.tsx
+++ apps/web/src/modules/ads/AdsWorkspaceWithMeta.tsx
@@ -1,10 +1,12 @@
 import { MetaAdsInsightsPanel } from './MetaAdsInsightsPanel';
+import { TikTokAdsCampaigns } from './TikTokAdsCampaigns';
 import { AdsWorkspace } from './AdsWorkspace';
 
 export function AdsWorkspaceWithMeta() {
   return (
     <div className="ads-workspace-with-meta">
       <MetaAdsInsightsPanel />
+      <TikTokAdsCampaigns />
       <AdsWorkspace />
     </div>
   );
