3c9750f910afb0c1f6aacb9e652b1c4e2bf64f27
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-10 04:08:08 +0500

    refactor: replace analytics DOM cleanup with CSS

--- src/components/AnalyticsWorkspace.tsx
+++ src/components/AnalyticsWorkspace.tsx
@@ -1,10 +1,9 @@
 import V36Dashboard from './V36Dashboard';
 import DetailedConversionMatrices from './DetailedConversionMatrices';
-import RemoveLegacyConversionCards from './RemoveLegacyConversionCards';
+import '../analytics-workspace.css';
 
 export default function AnalyticsWorkspace() {
   return <div className="stack">
-    <RemoveLegacyConversionCards />
     <V36Dashboard />
     <DetailedConversionMatrices days={7} />
   </div>;
