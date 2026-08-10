0d453160557874d989ea3065a052d9c51ff7e9f7
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-10 23:33:29 +0500

    refactor: remove embedded Google integrations island

--- src/pages/IntegrationsWorkspace.tsx
+++ src/pages/IntegrationsWorkspace.tsx
@@ -1,19 +1,5 @@
 import IntegrationManager from '../components/IntegrationManager';
-import { GoogleIntegrationsPage } from './StrategicPlatformPages';
-import '../integrations-workspace.css';
 
 export default function IntegrationsWorkspace() {
-  return <div className="integrations-workspace">
-    <IntegrationManager />
-    <section className="integrations-google-embedded" aria-label="Google integrations">
-      <div className="integrations-google-divider">
-        <span>GOOGLE</span>
-        <div>
-          <h2>Google Ads и Google Analytics 4</h2>
-          <p>Google-подключения являются частью общего каталога интеграций. Здесь настраиваются OAuth, аккаунты, свойства и синхронизация.</p>
-        </div>
-      </div>
-      <GoogleIntegrationsPage />
-    </section>
-  </div>;
+  return <IntegrationManager />;
 }
