a0ec422c651e420896922fa8b9623af9061a9519
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-11 05:07:50 +0500

    feat: show inbound Zadarma controls

--- src/pages/IntegrationsWorkspace.tsx
+++ src/pages/IntegrationsWorkspace.tsx
@@ -1,9 +1,11 @@
 import IntegrationManager from '../components/IntegrationManager';
 import ZadarmaIntegrationPanel from '../components/ZadarmaIntegrationPanel';
+import ZadarmaInboundControls from '../components/ZadarmaInboundControls';
 
 export default function IntegrationsWorkspace() {
   return <>
     <ZadarmaIntegrationPanel />
+    <ZadarmaInboundControls />
     <IntegrationManager />
   </>;
 }
