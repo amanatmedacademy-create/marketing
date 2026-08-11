949638881afb8ce45007c7b7b9f1aecb5f56321a
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-11 23:10:26 +0500

    Load final mobile responsive layer

--- src/main.tsx
+++ src/main.tsx
@@ -26,6 +26,7 @@ import './imds-color-system-final.css';
 import './imds-light-reference.css';
 import './imds-deal-workspace-theme.css';
 import './imds-global-theme-coverage.css';
+import './imds-mobile.css';
 
 type ThemeMode = 'light' | 'dark';
 
