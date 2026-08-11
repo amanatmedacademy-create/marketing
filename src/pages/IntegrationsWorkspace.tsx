e459290d1ed66f673b30fe4f9e300359076995da
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-11 06:04:03 +0500

    feat: expose MIS integration workspace

--- src/pages/IntegrationsWorkspace.tsx
+++ src/pages/IntegrationsWorkspace.tsx
@@ -1,9 +1,11 @@
 import IntegrationManager from '../components/IntegrationManager';
+import MisIntegrationPanel from '../components/MisIntegrationPanel';
 import ZadarmaIntegrationPanel from '../components/ZadarmaIntegrationPanel';
 import ZadarmaInboundControls from '../components/ZadarmaInboundControls';
 
 export default function IntegrationsWorkspace() {
   return <>
+    <MisIntegrationPanel />
     <ZadarmaIntegrationPanel />
     <ZadarmaInboundControls />
     <IntegrationManager />
