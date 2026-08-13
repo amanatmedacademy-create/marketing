887360a7808f2ebe02f582f0ad4cd0f8f24cd92e
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-13 12:31:51 +0500

    refactor(telephony): merge modes and dialer into compact toolbar

--- src/pages/TelephonyPage.tsx
+++ src/pages/TelephonyPage.tsx
@@ -7,14 +7,14 @@ import '../telephony-layout-hotfix.css';
 export default function TelephonyPage() {
   return (
     <div className="telephony-v5">
-      <div className="telephony-v5__top">
+      <div className="telephony-v5__toolbar">
+        <TelephonyDialer />
         <div className="telephony-v5__modes" aria-label="Режимы телефонии">
-          <button type="button" className="active"><Headphones size={14}/>Оператор</button>
-          <button type="button" disabled title="Supervisor workspace будет отдельным режимом"><Radio size={14}/>Supervisor</button>
-          <button type="button" disabled title="Analytics workspace будет отдельным режимом"><BarChart3 size={14}/>Аналитика</button>
+          <button type="button" className="active"><Headphones size={13}/>Оператор</button>
+          <button type="button" disabled title="Supervisor workspace будет отдельным режимом"><Radio size={13}/>Supervisor</button>
+          <button type="button" disabled title="Analytics workspace будет отдельным режимом"><BarChart3 size={13}/>Аналитика</button>
         </div>
       </div>
-      <TelephonyDialer />
       <TelephonyOperatorWorkspace />
     </div>
   );
