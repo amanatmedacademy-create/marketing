17d82d8bd273d4acfb1539fc3efb71a642845939
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-18 18:33:41 +0500

    feat(binotel): complete OAuth return on integrations route

--- src/pages/IntegrationsWorkspace.tsx
+++ src/pages/IntegrationsWorkspace.tsx
@@ -1,10 +1,14 @@
+import BinotelOAuthReturnHandler from '../components/BinotelOAuthReturnHandler';
 import IntegrationManager from '../components/IntegrationManager';
 import OperationalIntegrationCards from '../components/OperationalIntegrationCards';
 import '../integration-workspace-unified.css';
 
 // Keep the integrations route on the dedicated VPS frontend release path.
 export default function IntegrationsWorkspace() {
-  return <IntegrationManager>
-    <OperationalIntegrationCards inline />
-  </IntegrationManager>;
+  return <>
+    <BinotelOAuthReturnHandler />
+    <IntegrationManager>
+      <OperationalIntegrationCards inline />
+    </IntegrationManager>
+  </>;
 }
