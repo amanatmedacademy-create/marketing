5cf68d5cf681b650b459f24205f2c8b8c6769300
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-18 10:52:10 +0500

    feat(analytics): surface data quality in marketing analytics

--- src/components/AnalyticsWorkspace.tsx
+++ src/components/AnalyticsWorkspace.tsx
@@ -1,9 +1,11 @@
+import AnalyticsDataQualityPanel from './AnalyticsDataQualityPanel';
 import V36Dashboard from './V36Dashboard';
 import DetailedConversionMatrices from './DetailedConversionMatrices';
 import '../analytics-workspace.css';
 
 export default function AnalyticsWorkspace() {
   return <div className="stack">
+    <AnalyticsDataQualityPanel />
     <V36Dashboard />
     <DetailedConversionMatrices days={7} />
   </div>;
